32d099b2dec8589615676cd815da83787e840362
Centrix14 <artiom.zvyagin@yandex.ru>
2021-02-22 08:56:42 +0300

    parser done

--- pars.c
+++ pars.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "st.h/st.h"
 #include "pars.h"
+#include "list_builder.h"
 
 int main() {
+	lb_init();
+
 	pl_open_file("sample.msq");
+	lb_show_list();
+
+	lb_free();
 	return 0;
 }
 
@@ -73,12 +80,32 @@ void pl_check_str(pl_state *ps) {
 }
 
 void pl_check_space(pl_state *ps) {
-	if (ps->c == ' ') {
+	if (isspace(ps->c)) {
 		ps->buffer[ps->i++] = 0;
 
-		printf("Tok: %s\n", ps->buffer);
-		ps->i = 0;
+		pl_add_token(ps);
 		ps->c = 0;
+	}
+	else
+		pl_check_delimetrs(ps);
+}
+
+void pl_check_delimetrs(pl_state *ps) {
+	if (ps->c == ';') {
+		ps->buffer[ps->i++] = 0;
+
+		pl_add_token(ps);
+		ps->c = 0;
+	}
+	else if (strchr("[]", ps->c)) {
+		ps->buffer[ps->i++] = 0;
+
+		pl_add_token(ps);
+
+		ps->buffer[ps->i++] = ps->c;
+		ps->buffer[ps->i] = 0;
+
+		pl_add_token(ps);
 	}
 	else
 		pl_check_symbol(ps);
@@ -87,11 +114,21 @@ void pl_check_space(pl_state *ps) {
 void pl_check_symbol(pl_state *ps) {
 	if (isalnum(ps->c) || ispunct(ps->c))
 		ps->buffer[ps->i++] = ps->c;
-	else if (isspace(ps->c))
-		;
 	else {
 		pl_free_buf(ps);
 
 		st_err("unknown symbol");
 	}
+}
+
+void pl_show_token(pl_state *ps) {
+	if (strlen(ps->buffer))
+		printf("Tok: %s\n", ps->buffer);
+}
+
+void pl_add_token(pl_state *ps) {
+	if (strlen(ps->buffer))
+		lb_add_token(ps);
+
+	ps->i = 0;
 }
