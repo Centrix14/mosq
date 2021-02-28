79ab5fa5838184324545ee024889beb75fa2f5b7
Centrix14 <artiom.zvyagin@yandex.ru>
2021-02-28 10:48:49 +0300

    some update

---
+++ interp.h
@@ -0,0 +1,15 @@
+#ifndef interp
+#define interp
+
+void il_eval(list *node);
+void il_eval_as_func(char *tok, list *node);
+
+void il_run(int code, list *node);
+
+int il_is_block(char *tok);
+int il_is_std_func(char *tok);
+
+void il_cb_print(list *node);
+void il_cb_help(list *node);
+
+#endif
