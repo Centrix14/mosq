1dc570d43a6cdfb519eab6d998bf8fd7c63be68a
Centrix14 <artiom.zvyagin@yandex.ru>
2021-02-28 10:48:17 +0300

    some update

--- list_builder.h
+++ list_builder.h
@@ -1,10 +1,17 @@
 #ifndef list_builder
 #define list_builder
 
+#include "tl2/list.h"
+#include "pars.h"
+
 void lb_init();
 void lb_free();
 
 void lb_add_token(pl_state *ps);
 void lb_show_list();
+
+void lb_crawl(void (*func)(list*));
+
+void lb_print_node(list *node);
 
 #endif
