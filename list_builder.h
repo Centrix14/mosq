d6f583719a1754ea0b2676cb0dece0e60947490e
Centrix14 <artiom.zvyagin@yandex.ru>
2021-03-22 16:36:59 +0300

    remove some fuction

--- list_builder.h
+++ list_builder.h
@@ -8,20 +8,15 @@ void lb_init();
 void lb_free();
 
 void lb_add_token(pl_state *ps);
-void lb_add_block(pl_state *ps);
+void lb_insert_in_list(list *lptr, char *str);
 
 void lb_show_list();
+void lb_print_node(list *node);
 
 void lb_crawl(void (*func)(list*));
 
-void lb_print_node(list *node);
-
 char *lb_eval_addr(char *addr, list *node);
-
 int lb_is_addr_expr(char *expr);
-
-void lb_insert_in_list(list *lptr, char *str);
-
 void lb_expand_addr(list *node, char *addr);
 
 #endif
